a8767cdb132cea678660e527fbcadd3836584906
Tomas Zaoral <zaoralt@gmail.com>
2016-05-25 10:51:32 +0100

    implemented SMA

--- src/tapassage/core.clj
+++ src/tapassage/core.clj
@@ -1,13 +1,21 @@
-(ns tapassage.core)
+(ns tapassage.core
+  (:import (clojure.lang PersistentQueue)))
 
 
 (defn sma [period]
   (fn [xf]
-    (let [called (volatile! 0)]
+    (let [values (volatile! PersistentQueue/EMPTY)
+          sum (volatile! 0.0)]
       (fn
         ([] (xf))
         ([result] (xf result))
         ([result input]
-         (if (<= (vswap! called inc) 88)
-           (xf result input)
+         (vswap! sum + input)
+         (vswap! values conj input)
+         (when (> (count @values) period)
+           (vswap! sum - (first @values))
+           (vswap! values pop))
+         (if (= (count @values) period)
+           (xf result (/ @sum period))
            result))))))
+
--- test/tapassage/core_test.clj
+++ test/tapassage/core_test.clj
@@ -1,19 +1,25 @@
 (ns tapassage.core-test
   (:require [clojure.test :refer :all]
-            [tapassage.core :as tp])
+            [tapassage.core :as tp]
+            [clojure.algo.generic.math-functions :refer [approx=]])
   (:import [com.tictactec.ta.lib Core MInteger RetCode]))
 
-(def ^:private total-periods 100)
-(def ^:private ma-period 13)
+(def ^:private total-periods 50)
+(def ^:private ma-period 5)
 (def ^:private input (double-array (repeatedly total-periods rand)))
+
+(defn seq-approx= [xs ys eps]
+  (doseq [[x y] (map vector xs ys)]
+    (is (approx= x y eps))))
 
 (deftest test-sma
   (testing "SMA"
     (let [out-start-idx (new MInteger)
           out-len (new MInteger)
           output (double-array total-periods)
-          ret-code (.. (new Core) (sma 0 (dec total-periods) input ma-period out-start-idx out-len output))
-          tp-sma (into [] (tp/sma ma-period) input)]
+          ret-code (.. (new Core) (sma 0 (dec total-periods) input ma-period
+                                       out-start-idx out-len output))
+          tp-sma (sequence (tp/sma ma-period) input)
+          ta-sma (take (.-value out-len) (seq output))]
       (is (= ret-code RetCode/Success))
-      (println (.-value out-len))
-      (is (= (count tp-sma) out-len)))))
+      (seq-approx= tp-sma ta-sma 1e-15))))
