95c3fafc8a4a081e8c5a4d7597181b3ec3a58f9a
Tomas Zaoral <zaoralt@gmail.com>
2016-05-25 18:03:23 +0100

    implemented ema

--- src/tapassage/core.clj
+++ src/tapassage/core.clj
@@ -19,3 +19,19 @@
            (xf result (/ @sum period))
            result))))))
 
+(defn ema [period]
+  (fn [xf]
+    (let [prev-ema (volatile! [])
+          alpha (/ 2 (+ period 1))]
+      (fn
+        ([] (xf))
+        ([result] (xf result))
+        ([result input]
+         (let [ema-f (fn [p] (->> p (- input) (* alpha) (+ p)))
+               step (fn [new-ema] (vreset! prev-ema new-ema) (xf result new-ema))]
+           (if (vector? @prev-ema)
+             (do (vswap! prev-ema conj input)
+                 (if (= (count @prev-ema) period)
+                   (step (/ (apply + @prev-ema) period))
+                   result))
+             (step (ema-f @prev-ema)))))))))
--- test/tapassage/core_test.clj
+++ test/tapassage/core_test.clj
@@ -7,10 +7,10 @@
 (def ^:private total-periods 50)
 (def ^:private ma-period 5)
 (def ^:private input (double-array (repeatedly total-periods rand)))
+(def ^:private epsilon 1e-15)
 
-(defn seq-approx= [xs ys eps]
-  (doseq [[x y] (map vector xs ys)]
-    (is (approx= x y eps))))
+(defn seq-approx= [xs ys]
+  (every? (fn [[x y]] (approx= x y epsilon)) (map vector xs ys)))
 
 (deftest test-sma
   (testing "SMA"
@@ -22,4 +22,18 @@
           tp-sma (sequence (tp/sma ma-period) input)
           ta-sma (take (.-value out-len) (seq output))]
       (is (= ret-code RetCode/Success))
-      (seq-approx= tp-sma ta-sma 1e-15))))
+      (is (= (count tp-sma) (count ta-sma)))
+      (is (seq-approx= tp-sma ta-sma)))))
+
+(deftest test-ema
+  (testing "EMA"
+    (let [out-start-idx (new MInteger)
+          out-len (new MInteger)
+          output (double-array total-periods)
+          ret-code (.. (new Core) (ema 0 (dec total-periods) input ma-period
+                                       out-start-idx out-len output))
+          tp-ema (sequence (tp/ema ma-period) input)
+          ta-ema (take (.-value out-len) (seq output))]
+      (is (= ret-code RetCode/Success))
+      (is (= (count tp-ema) (count ta-ema)))
+      (is (seq-approx= tp-ema ta-ema)))))
