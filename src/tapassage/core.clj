b0b9611f6c8e75fd62c623fe58924fcfaa7467a4
Tomas Zaoral <zaoralt@gmail.com>
2016-05-26 17:45:29 +0100

    implemented roc

--- src/tapassage/core.clj
+++ src/tapassage/core.clj
@@ -1,7 +1,6 @@
 (ns tapassage.core
   (:import (clojure.lang PersistentQueue)))
 
-
 (defn- xfhcomp [& xfs]
   (fn [xf]
     (let [ixf (fn [_ input] input)
@@ -15,7 +14,9 @@
              (xf result rs)
              result)))))))
 
-(defn sma [period]
+;; MOVING AVERAGES
+
+(defn sma [p]
   (fn [xf]
     (let [values (volatile! PersistentQueue/EMPTY)
           sum (volatile! 0.0)]
@@ -25,17 +26,17 @@
         ([result input]
          (vswap! sum + input)
          (vswap! values conj input)
-         (when (> (count @values) period)
+         (when (> (count @values) p)
            (vswap! sum - (first @values))
            (vswap! values pop))
-         (if (= (count @values) period)
-           (xf result (/ @sum period))
+         (if (= (count @values) p)
+           (xf result (/ @sum p))
            result))))))
 
-(defn ema [period]
+(defn ema [p]
   (fn [xf]
     (let [prev-ema (volatile! [])
-          alpha (/ 2 (+ period 1))]
+          alpha (/ 2 (+ p 1))]
       (fn
         ([] (xf))
         ([result] (xf result))
@@ -47,33 +48,33 @@
            (if (vector? @prev-ema)
              (do
                (vswap! prev-ema conj input)
-               (if (= (count @prev-ema) period)
-                 (step (/ (apply + @prev-ema) period))
+               (if (= (count @prev-ema) p)
+                 (step (/ (apply + @prev-ema) p))
                  result))
              (step (ema-f @prev-ema)))))))))
 
-(defn dema [period]
+(defn dema [p]
   (comp
-    (ema period)
+    (ema p)
     (xfhcomp
       (map (partial * 2))
-      (ema period))
+      (ema p))
     (map (partial apply -))))
 
-(defn tema [period]
+(defn tema [p]
   (comp
-    (ema period)
+    (ema p)
     (xfhcomp
       (map identity)
-      (ema period))
+      (ema p))
     (xfhcomp
       (comp (map first) (map (partial * 3)))
       (comp (map second) (map (partial * 3)))
-      (comp (map second) (ema period)))
+      (comp (map second) (ema p)))
     (map (fn [[ema ema2 ema3]] (+ ema3 (- ema ema2))))))
 
-(defn wma [period]
-  (let [triangles (range 1 (inc period))
+(defn wma [p]
+  (let [triangles (range 1 (inc p))
         denominator (double (apply + triangles))
         weights (map #(/ % denominator) triangles)]
     (fn [xf]
@@ -83,9 +84,25 @@
           ([result] (xf result))
           ([result input]
            (vswap! values conj input)
-           (if (< (count @values) period)
+           (if (< (count @values) p)
              result
              (do
-               (when (> (count @values) period)
+               (when (> (count @values) p)
                  (vswap! values pop))
                (xf result (apply + (map * weights @values)))))))))))
+
+;; OSCILATORS
+
+(defn roc [p]
+  (fn [xf]
+    (let [values (volatile! PersistentQueue/EMPTY)]
+      (fn
+        ([] (xf))
+        ([result] (xf result))
+        ([result input]
+         (vswap! values conj input)
+         (if (<= (count @values) p)
+           result
+           (let [prev (first @values)]
+             (vswap! values pop)
+             (xf result (* 100 (dec (/ input prev)))))))))))
--- test/tapassage/core_test.clj
+++ test/tapassage/core_test.clj
@@ -5,9 +5,9 @@
   (:import [com.tictactec.ta.lib Core MInteger RetCode]))
 
 (def ^:private total-periods 100)
-(def ^:private ma-period 13)
+(def ^:private p 13)
 (def ^:private input (double-array (repeatedly total-periods rand)))
-(def ^:private epsilon 1e-15)
+(def ^:private epsilon 1e-14)
 
 (defn seq-approx= [xs ys]
   (every? (fn [[x y]] (approx= x y epsilon)) (map vector xs ys)))
@@ -17,9 +17,8 @@
     (let [out-start-idx (new MInteger)
           out-len (new MInteger)
           output (double-array total-periods)
-          ret-code (.. (new Core) (sma 0 (dec total-periods) input ma-period
-                                       out-start-idx out-len output))
-          tp-sma (sequence (tp/sma ma-period) input)
+          ret-code (.. (new Core) (sma 0 (dec total-periods) input p out-start-idx out-len output))
+          tp-sma (sequence (tp/sma p) input)
           ta-sma (take (.-value out-len) (seq output))]
       (is (= ret-code RetCode/Success))
       (is (= (count tp-sma) (count ta-sma)))
@@ -30,9 +29,8 @@
     (let [out-start-idx (new MInteger)
           out-len (new MInteger)
           output (double-array total-periods)
-          ret-code (.. (new Core) (ema 0 (dec total-periods) input ma-period
-                                       out-start-idx out-len output))
-          tp-ema (sequence (tp/ema ma-period) input)
+          ret-code (.. (new Core) (ema 0 (dec total-periods) input p out-start-idx out-len output))
+          tp-ema (sequence (tp/ema p) input)
           ta-ema (take (.-value out-len) (seq output))]
       (is (= ret-code RetCode/Success))
       (is (= (count tp-ema) (count ta-ema)))
@@ -43,9 +41,8 @@
     (let [out-start-idx (new MInteger)
           out-len (new MInteger)
           output (double-array total-periods)
-          ret-code (.. (new Core) (dema 0 (dec total-periods) input ma-period
-                                        out-start-idx out-len output))
-          tp-dema (sequence (tp/dema ma-period) input)
+          ret-code (.. (new Core) (dema 0 (dec total-periods) input p out-start-idx out-len output))
+          tp-dema (sequence (tp/dema p) input)
           ta-dema (take (.-value out-len) (seq output))]
       (is (= ret-code RetCode/Success))
       (is (= (count tp-dema) (count ta-dema)))
@@ -56,23 +53,33 @@
     (let [out-start-idx (new MInteger)
           out-len (new MInteger)
           output (double-array total-periods)
-          ret-code (.. (new Core) (tema 0 (dec total-periods) input ma-period
-                                        out-start-idx out-len output))
-          tp-tema (sequence (tp/tema ma-period) input)
+          ret-code (.. (new Core) (tema 0 (dec total-periods) input p out-start-idx out-len output))
+          tp-tema (sequence (tp/tema p) input)
           ta-tema (take (.-value out-len) (seq output))]
       (is (= ret-code RetCode/Success))
       (is (= (count tp-tema) (count ta-tema)))
-      (is (= (seq-approx= tp-tema ta-tema))))))
+      (is (seq-approx= tp-tema ta-tema)))))
 
 (deftest test-wma
   (testing "WMA"
     (let [out-start-idx (new MInteger)
           out-len (new MInteger)
           output (double-array total-periods)
-          ret-code (.. (new Core) (wma 0 (dec total-periods) input ma-period
-                                       out-start-idx out-len output))
-          tp-wma (sequence (tp/wma ma-period) input)
+          ret-code (.. (new Core) (wma 0 (dec total-periods) input p out-start-idx out-len output))
+          tp-wma (sequence (tp/wma p) input)
           ta-wma (take (.-value out-len) (seq output))]
       (is (= ret-code RetCode/Success))
       (is (= (count tp-wma) (count ta-wma)))
-      (is (= (seq-approx= tp-wma ta-wma))))))
+      (is (seq-approx= tp-wma ta-wma)))))
+
+(deftest test-roc
+  (testing "ROC"
+    (let [out-start-idx (new MInteger)
+          out-len (new MInteger)
+          output (double-array total-periods)
+          ret-code (.. (new Core) (roc 0 (dec total-periods) input p out-start-idx out-len output))
+          tp-roc (sequence (tp/roc p) input)
+          ta-roc (take (.-value out-len) (seq output))]
+      (is (= ret-code RetCode/Success))
+      (is (= (count tp-roc) (count ta-roc)))
+      (is (seq-approx= tp-roc ta-roc)))))
