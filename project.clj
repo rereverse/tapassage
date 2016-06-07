fac06d5be5a01788ac22876a99360143865dc542
Tomas Zaoral <zaoralt@gmail.com>
2016-06-07 09:12:36 +0100

    adding ahead of time compilation

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.tictactec/ta-lib "0.4.0"]
-                 [org.clojure/algo.generic "0.1.2"]])
+                 [org.clojure/algo.generic "0.1.2"]]
+  :aot :all)
